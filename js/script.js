8cbf15eb8e34967756ff82a6285c3fba26e52c1e
rokemagik <emidqtrabajo@gmail.com>
2025-08-04 12:35:03 -0300

    Creando calculadora

---
+++ js/script.js
@@ -0,0 +1,83 @@
+
+
+let pantallaValor = document.getElementById("calculo")
+let pantallaResultado = document.getElementById("resultado")
+
+const historial = [];
+const historialMax = 6;
+
+let numero = 1; 
+
+let botones = document.querySelectorAll("button")
+botones.forEach(function(boton) {
+    boton.addEventListener("click", () => {
+
+        let botonValor = boton.dataset.valor;
+        let botonAccion = boton.dataset.accion;
+        let botonOperacion = boton.dataset.operacion;
+
+        if(botonValor) {
+            if(pantallaValor.textContent.length <= 21) {
+                pantallaValor.textContent += botonValor
+            }
+        } else if(botonAccion) {
+            switch(boton.dataset.accion) {
+                case "clear":
+                    Clear();
+                    break
+                case "delete":
+                    Delete();
+                    break
+                case "igual":
+                    Resultado();
+                    break
+            }
+        } else if(botonOperacion) {
+            if(pantallaValor.textContent.length <= 21) {
+                pantallaValor.textContent += botonOperacion;
+            }
+        } else {
+            alert("Opcion no Valida");
+        }
+    })
+})
+
+function Clear() {
+    if(pantallaValor.textContent.length || pantallaResultado.textContent.length) {
+        pantallaValor.textContent = "";
+        pantallaResultado.textContent = "";
+    }
+}
+
+function Delete() {
+    if (pantallaValor.textContent.length) {
+        pantallaValor.textContent = pantallaValor.textContent.slice(0, -1);
+    }
+}
+
+function Resultado() {
+    let operacion = pantallaValor.textContent;
+    let resultado = math.evaluate(operacion);
+
+    pantallaResultado.textContent = resultado;
+
+    //while (historial.length >= historialMax) {
+    //    historial.shift()
+    //} 
+
+    historial.push(`${numero++}. ${operacion} = ${resultado}`);
+    actualizarHistorial();
+}
+
+function actualizarHistorial() {
+    const contenedor = document.getElementById("operaciones");
+    contenedor.innerHTML = "";
+
+    historial.forEach((item) => {
+        const p = document.createElement("p");
+        p.textContent = item;
+        contenedor.appendChild(p);
+    })
+
+    contenedor.scrollTop = contenedor.scrollHeight;
+}
